7ba401aea39c3ac749f3858388fdc5005a35bda4
Manu-Vz <manuvasquez2004@gmail.com>
2023-08-07 12:22:02 -0300

    Se Agrega La Clase Colaborador
    Objetivo: Escribir el nombre de los/as/es colaboradores/as para completar el ejercicio 2 de la Guía 1.

--- src/ejemploversiones/EjemploVersiones.java
+++ src/ejemploversiones/EjemploVersiones.java
@@ -3,7 +3,8 @@ package ejemploversiones;
 public class EjemploVersiones {
 
     public static void main(String[] args) {
-        
+        Colaborador primero = new Colaborador("Manuel");
+        Colaborador segundo = new Colaborador();
     }
     
 }
