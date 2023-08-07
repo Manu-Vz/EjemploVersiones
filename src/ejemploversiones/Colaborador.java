27fcc838366643b67dd73c365e09a11b44d7d7c1
Manu-Vz <manuvasquez2004@gmail.com>
2023-08-07 12:22:35 -0300

    Constructor de la Clase Colaborador

---
+++ src/ejemploversiones/Colaborador.java
@@ -0,0 +1,21 @@
+package ejemploversiones;
+
+public class Colaborador {
+    private String nombre;
+
+    public Colaborador(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Colaborador(){}
+    
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    
+}
